91fc9891aa824bc035be700c00fb4f5ec6ed488d
2531yasuaki@gmail.com <2531yasuaki@gmail.com>
2023-02-09 00:01:10 +0900

    3.2節まで

---
+++ sample/2-3/cbow_predict.py
@@ -0,0 +1,27 @@
+import sys
+
+sys.path.append(".")
+import numpy as np
+
+from common.layers import MatMul
+
+# サンプルのコンテキストデータ
+context0 = np.array([[1, 0, 0, 0, 0, 0, 0]])
+context1 = np.array([[0, 0, 1, 0, 0, 0, 0]])
+
+# 重みの初期化
+weight_in = np.random.randn(7, 3)
+weight_out = np.random.randn(3, 7)
+
+# レイヤの生成
+in_layer0 = MatMul(weight_in)
+in_layer1 = MatMul(weight_in)
+out_layer = MatMul(weight_out)
+
+# 順伝播
+h0 = in_layer0.forward(context0)
+h1 = in_layer1.forward(context1)
+h = 0.5 * (h0 + h1)
+s = out_layer.forward(h)
+
+print(s)
